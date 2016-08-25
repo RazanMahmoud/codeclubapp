6d006a62479677e9af6e968df3db4b7d49ddf496
RazanMahmoud <razmaht9@gmail.com>
2016-08-25 13:02:52 -0700

    no message

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var session = require('express-session');
 var Event = require('../model/users');
-var User = require('../model/users');
+//var User = require('../model/users');
 
 // Get Homepage
 router.get('/', ensureAuthenticated, function(req, res){
